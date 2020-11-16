e8c4695cfed70573e6c59220904110bf6fd4bab2
Joao Gaspar <joaopedrohenriquesgaspar@gmail.com>
2020-11-16 18:14:14 +0000

    First Commit

---
+++ scripts/data.js
@@ -0,0 +1,74 @@
+const projects = [{
+        name: "Positive Vibes",
+        link: "https://www.behance.net/gallery/101081949/Sumol-Positive-Vibes",
+        info: "ZWELA 2020",
+        image: "sumol.png"
+    },
+    {
+        name: "Let´s Talk Energys",
+        link: "https://www.behance.net/gallery/100228999/Lets-Talk-Energy-InnoEnergy",
+        info: "ZWELA 2020",
+        image: "innoenergy.png"
+    },
+    {
+        name: "infloortech",
+        link: "https://www.behance.net/gallery/99653643/infloortech-Revestimentos-Tcnicos",
+        info: "2020",
+        image: "ift.png"
+    },
+    {
+        name: "Oxigen",
+        link: "https://www.behance.net/gallery/95413425/Oxigen-Grafica-Digital",
+        info: "2020",
+        image: "oxigen.png"
+    },
+    {
+        name: "Carga Magazine",
+        link: "https://www.behance.net/gallery/100307309/Carga-Magazine-Website",
+        info: "ZWELA 2018",
+        image: "carga.png"
+    },
+    {
+        name: "Space Inverters",
+        link: "https://www.behance.net/gallery/42813611/Space-Inverters-Website",
+        info: "ZWELA 2018",
+        image: "space.png"
+    },
+    {
+        name: "Bold Agency",
+        link: "https://www.behance.net/gallery/42881123/Bold-Agency-Website",
+        info: "HOJI 2018",
+        image: "bold.gif"
+    },
+    {
+        name: "EDP Hello Christmas",
+        link: "https://www.behance.net/gallery/42815515/EDP-Hello-Christmas-Microsite",
+        info: "HOJI 2018",
+        image: "edp.png"
+    },
+    {
+        name: "STREETART Magazine",
+        link: "https://www.behance.net/gallery/42890313/STREETART-Magazine",
+        info: "2014",
+        image: "streeatART_2.png"
+    },
+    {
+        name: "EurOcean",
+        link: "https://www.behance.net/gallery/42851079/EurOcean-Website",
+        info: "HOJI 2016",
+        image: "eurocean.png"
+    },
+    {
+        name: "Gameboys Exhibition Studio",
+        link: "https://www.behance.net/gallery/43058305/Gameboys-Exhibition-Studio",
+        info: "GMBS 2014",
+        image: "gmbs.png"
+    },
+    {
+        name: "Olivia",
+        link: "https://www.behance.net/gallery/42868555/Olivia-APPm",
+        info: "ZWELA 2017",
+        image: "olivia.png"
+    }
+
+]
---
+++ scripts/scripts.js
@@ -0,0 +1,60 @@
+// word animation
+var my_interval;
+$(document).on("mouseenter", "#random_text", function() {
+    var random_words = ["Digital", "Graphic", "UI/UX"]
+
+    var i = 0;
+
+    my_interval = setInterval(function() {
+        $("#random_text").html(random_words[i])
+
+        if (i == random_words.length - 1)
+            i = 0
+        else
+            i++
+
+    }, 400)
+}).mouseout(function() {
+    $("#random_text").html("¯&#92;_(ツ)_/¯")
+    clearInterval(my_interval)
+});
+
+// hover img data
+$(projects).each(function(i, item) {
+
+    var row_element = '<div class="row">' +
+        '<div class="col-sm-12">' +
+        '<h2><a href="' + item.link + '" target="_blank" class="textinfo">' + item.name + '</a></h2>' +
+        '<p class="hideinfo blue_type">' + item.info + '</p>' +
+        '<div class="mouseFollow"> <img src = "images/' + item.image + '" >' +
+        '</div></div></div>';
+    console.log(row_element)
+    $("#info_container").append(row_element)
+
+})
+
+// hover image
+$(document).on('mousemove', '.textinfo', function(e) {
+
+    $(this).closest('.col-sm-12').find('.mouseFollow').show()
+    $(this).closest('.col-sm-12').find(".hideinfo").css("opacity", 1)
+
+    $(this).closest('.col-sm-12').find('.mouseFollow').css("top", e.clientY).css("left", e.clientX);
+}).mouseout(function() {
+    $(".mouseFollow").hide()
+    $(".hideinfo").css("opacity", 0)
+});
+
+// text TAB
+msg = "Hi there, my name is João Gaspar. I’m a ¯\_(ツ)_/¯ Designer based in Lisbon, PT. ";
+position = 0;
+
+function scrolltitle() {
+    document.title = msg.substring(position, msg.length) + msg.substring(0, position);
+    position++;
+    if (position > msg.length) position = 0
+    window.setTimeout("scrolltitle()", 100);
+}
+scrolltitle();
+
+document.cookie = "1+1=2"
